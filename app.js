5fdad0880e9c59696bff71ab9b06a94db85521af
revelation4825-tech <revelation4.8.25@gmail.com>
2026-02-13 17:02:01 -0700

    Added the search functionality using API

--- app.js
+++ app.js
@@ -6,3 +6,25 @@
       }
       let searchForm = document.querySelector("#search-form");
         searchForm.addEventListener("submit", search);
+
+    let apiKey = "203adfb889341eafafcd3o792bteb01b";
+
+    function displayTemperature(response) {
+        let currentTemperature = document.querySelector("#current-temperature-value");
+        console.log(response)
+        currentTemperature.innerHTML = Math.round(response.data.temperature.current);
+      }
+
+    function searchCity(event) {
+        event.preventDefault();
+
+        let cityInput = document.querySelector("#search-input");
+        let city = cityInput.value.trim();
+
+        let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+        axios.get(apiUrl).then(displayTemperature);
+      }
+
+        let changeCity = document.querySelector("#search-form");
+        changeCity.addEventListener("submit", searchCity);
